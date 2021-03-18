fd474c91938c632c5ed72ddee7cc5a203361ef21
Benedikt Jóhannesson <bennigeir@users.noreply.github.com>
2021-03-18 15:56:34 +0000

    sample rate extraction added

--- tal_ru_test.py
+++ tal_ru_test.py
@@ -3,17 +3,19 @@ import wave
 
 from base64 import b64encode
 
-
 token = 'Bearer ak_Pa2KJXLmk8djBKmJLXM6D4ZGeoVQPE3q0Kw9p5RqvzOa7yl2gbWYA1rN04eyQBOG'
 obj = open('data/flight01_benedikt.wav','rb').read()
 obj64 = str(b64encode(obj).decode("utf-8"))
+
+wave_obj = wave.open('data/flight01_benedikt.wav','r')
+sample_rate = str(wave_obj.getframerate())
 
 headers = {
     'Content-Type': 'application/json',
     'Authorization': token,
 }
 
-data = '{"config": {"encoding": "LINEAR16", "sampleRate": 44100}, "audio" : {"content": "' + obj64 + '"}}'
+data = '{"config": {"encoding": "LINEAR16", "sampleRate": ' + sample_rate + '}, "audio" : {"content": "' + obj64 + '"}}'
 
 response = requests.post('https://tal.ru.is/v1/speech:syncrecognize',
                          headers=headers,

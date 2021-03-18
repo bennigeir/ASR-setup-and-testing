4f7bc805ea9dadb8ffd9e5c2bf5ebb6c342c918a
isidorag <70262109+isidorag@users.noreply.github.com>
2021-03-18 18:46:05 +0000

    updated

--- tal_ru_test.py
+++ tal_ru_test.py
@@ -1,24 +1,31 @@
 import requests
 import wave
+import glob
 
 from base64 import b64encode
 
-token = 'Bearer ak_Pa2KJXLmk8djBKmJLXM6D4ZGeoVQPE3q0Kw9p5RqvzOa7yl2gbWYA1rN04eyQBOG'
-obj = open('data/flight01_benedikt.wav','rb').read()
-obj64 = str(b64encode(obj).decode("utf-8"))
+file_list = glob.glob('data/isi/*.wav')   
+out_file = open('txt/sentences.output.txt','w',encoding='utf-8')
+for file in file_list:
+    token = 'Bearer ak_Pa2KJXLmk8djBKmJLXM6D4ZGeoVQPE3q0Kw9p5RqvzOa7yl2gbWYA1rN04eyQBOG'
+    obj = open(file,'rb').read()
+    obj64 = str(b64encode(obj).decode("utf-8"))
 
-wave_obj = wave.open('data/flight01_benedikt.wav','r')
-sample_rate = str(wave_obj.getframerate())
+    wave_obj = wave.open(file,'r')
+    sample_rate = str(wave_obj.getframerate())
 
-headers = {
-    'Content-Type': 'application/json',
-    'Authorization': token,
-}
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': token,
+        }
 
-data = '{"config": {"encoding": "LINEAR16", "sampleRate": ' + sample_rate + '}, "audio" : {"content": "' + obj64 + '"}}'
+    data = '{"config": {"encoding": "LINEAR16", "sampleRate": ' + sample_rate + '}, "audio" : {"content": "' + obj64 + '"}}'
 
-response = requests.post('https://tal.ru.is/v1/speech:syncrecognize',
-                         headers=headers,
-                         data=data)
+    response = requests.post('https://tal.ru.is/v1/speech:syncrecognize',
+                             headers=headers,
+                             data=data)
 
-print(response.text)
+    out_file.write(response.text[44:-6])
+    out_file.write('.\n')
+out_file.close()
+

22b2182919ad22dd2181bdbfc498e1dccdce8a89
Atli Snær <atlisa20@hi.is>
2021-03-21 21:06:08 +0000

    Speaker calculations implemented

--- count_errors.py
+++ count_errors.py
@@ -1,4 +1,5 @@
 import glob
+from typing import Dict
 import xml.etree.ElementTree as ET
 from collections import Counter
 from xml.dom import minidom 
@@ -41,21 +42,36 @@ def count_errors(file_list):
 
 def sort_errors(files):
     freq=Counter()
+    speaker_lis = []
     for filename in files:
+        freq_per_file = Counter()
         mydoc = minidom.parse(filename)
         items = mydoc.getElementsByTagName('error')
         for item in items:
             freq[item.attributes['xtype'].value]+=1
-    return freq
+            freq_per_file[item.attributes['xtype'].value]+=1
+        speaker_lis.append((filename, freq_per_file))
+    return freq, speaker_lis
+
+def calculate_word_error(errors:Dict, total_words:int):
+    total_errors = sum([value for key, value in errors.items() if key != "proper-name-fail"])
+    return total_errors/total_words
+    
 
 file_list = glob.glob('tei/*.xml')
 
 error_counts = count_errors(file_list)
+sorted_errors = sort_errors(file_list)
 
 print('Number of revision:', error_counts[2])
 print('Number of errors:', error_counts[3])
 print('Number of files:', error_counts[1])
 print("Number of words:", error_counts[0])
 print("Number of errors per 1000 words:", (error_counts[3]/error_counts[0])*1000)
-print("Number of errors per type:", sort_errors(file_list))
+print("Number of errors per type:", sorted_errors[0])
+print("\nSingle speaker errors:\n")
+for speaker in sorted_errors[1]:
+    print(f"Number of errors from {speaker[0]}", speaker[1])
+    print("\tWord error rate:",calculate_word_error(speaker[1], error_counts[0]))
+
 

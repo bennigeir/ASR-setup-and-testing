fc685ec07503e3cb3ac86b625d92d892928a4da7
Benedikt Jóhannesson <bennigeir@users.noreply.github.com>
2021-03-22 08:18:32 +0000

    cleanup and comment

--- tal_ru_test.py
+++ tal_ru_test.py
@@ -4,30 +4,59 @@ import glob
 
 from base64 import b64encode
 
+
+# Token and endpoint for the API call
+token = 'ak_Pa2KJXLmk8djBKmJLXM6D4ZGeoVQPE3q0Kw9p5RqvzOa7yl2gbWYA1rN04eyQBOG'
+endpoint = 'https://tal.ru.is/v1/speech:syncrecognize'
+
+# Folder containing the WAV files
 file_list = glob.glob('data/ben/*.wav')   
-token = 'Bearer ak_Pa2KJXLmk8djBKmJLXM6D4ZGeoVQPE3q0Kw9p5RqvzOa7yl2gbWYA1rN04eyQBOG'
+# Name of the file where response will be written to
+output_txt = 'txt/sentences_test.output.txt'
 
-out_file = open('txt/sentences.output.txt','w',encoding='utf-8')
-for file in sorted(file_list):
-    print(file)
+
+def main(verbose=False):
     
-    obj = open(file,'rb').read()
-    obj64 = str(b64encode(obj).decode("utf-8"))
+    # Open text file to write response from API
+    out_file = open(output_txt,'w',encoding='utf-8')
+    
+    # Iterate all WAV files in the directory
+    for file in sorted(file_list):
+        
+        if verbose:
+            print(file)
+        
+        # Open individual file, decode it as utf-8 and cast to base64
+        obj = open(file,'rb').read()
+        obj64 = str(b64encode(obj).decode("utf-8"))
+    
+        # Get sample rate of WAV file
+        wave_obj = wave.open(file,'r')
+        sample_rate = str(wave_obj.getframerate())
+    
+        # Define headers and data for the API call
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token,
+            }
+    
+        data = '{"config": {"encoding": "LINEAR16", "sampleRate": '\
+            + sample_rate + '}, "audio" : {"content": "' + obj64 + '"}}'
+    
+        # Make the API call
+        response = requests.post(endpoint,
+                                 headers=headers,
+                                 data=data)
+    
+        # Write selected parts of the response to a file
+        out_file.write(response.text[44:-6])
+        out_file.write('.\n')
+        
+    out_file.close()
+    
+    if verbose:
+        print('Done!')
 
-    wave_obj = wave.open(file,'r')
-    sample_rate = str(wave_obj.getframerate())
 
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': token,
-        }
-
-    data = '{"config": {"encoding": "LINEAR16", "sampleRate": ' + sample_rate + '}, "audio" : {"content": "' + obj64 + '"}}'
-
-    response = requests.post('https://tal.ru.is/v1/speech:syncrecognize',
-                             headers=headers,
-                             data=data)
-
-    out_file.write(response.text[44:-6])
-    out_file.write('.\n')
-out_file.close()
+if __name__ == "__main__":
+    main()
